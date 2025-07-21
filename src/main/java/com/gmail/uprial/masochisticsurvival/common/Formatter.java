9fdc82ba03ed9050ddbbe4a1ba0e182fb5f016d8
Ilya Roslyakov <i_roslyakov@wargaming.net>
2025-07-21 14:15:34 +0300

    Fixed debug info about players

--- src/main/java/com/gmail/uprial/masochisticsurvival/common/Formatter.java
+++ src/main/java/com/gmail/uprial/masochisticsurvival/common/Formatter.java
@@ -2,6 +2,7 @@ package com.gmail.uprial.masochisticsurvival.common;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 public class Formatter {
    public static String format(final Entity entity) {
@@ -9,6 +10,13 @@ public class Formatter {
                 entity.getType(),
                 entity.getWorld().getName(),
                 entity.getLocation().getX(), entity.getLocation().getY(), entity.getLocation().getZ());
+    }
+
+    public static String format(final Player player) {
+        return String.format("%s[%s:%.0f:%.0f:%.0f]",
+                player.getName(),
+                player.getWorld().getName(),
+                player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
     }
 
     public static String format(final Location location) {
