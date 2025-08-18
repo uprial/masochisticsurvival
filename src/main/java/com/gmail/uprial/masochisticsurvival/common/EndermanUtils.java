222e9140a46277b5c66919b6e06bf76660a5db91
Ilya Roslyakov <i_roslyakov@wargaming.net>
2025-08-18 21:29:50 +0300

    Fixed Nasty Endermans vertical space calculation

--- src/main/java/com/gmail/uprial/masochisticsurvival/common/EndermanUtils.java
+++ src/main/java/com/gmail/uprial/masochisticsurvival/common/EndermanUtils.java
@@ -6,8 +6,20 @@ import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Player;
 
 public class EndermanUtils {
+    /*
+        According to https://minecraft.wiki/w/Enderman,
+        Enderman height is 2.9 blocks.
+     */
+    private static final double HEIGHT = 2.9D;
+    /*
+        Must make Enderman height less than 2.0,
+        which means at most 2.0/2.9=0.689655.
+     */
+    private static final double BABY_SCALE = 0.6D;
+
     public static boolean isAppropriatePlayer(final Player player, final boolean baby) {
-        final double yHeadroom = baby ? 1.1D : 2.1D;
+        final double yHeadroom = baby ? (HEIGHT * BABY_SCALE) : HEIGHT;
+
         return (player.isValid())
                 && (player.getWorld()
                 .getBlockAt(player.getLocation().clone().add(0.0D, yHeadroom, 0.0D)).isPassable());
@@ -15,6 +27,6 @@ public class EndermanUtils {
 
     public static void setBaby(final Enderman enderman) {
         final AttributeInstance a = enderman.getAttribute(Attribute.SCALE);
-        a.setBaseValue(a.getBaseValue() * 0.6D);
+        a.setBaseValue(a.getBaseValue() * BABY_SCALE);
     }
 }
