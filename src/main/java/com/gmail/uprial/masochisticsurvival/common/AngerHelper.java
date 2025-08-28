30c19c615e1343ef9cbdfa19795aecb01883fa55
Ilya Roslyakov <i_roslyakov@wargaming.net>
2025-08-28 13:31:47 +0300

    Generalized AngerHelper.isSimulated - fixed misprnit

--- src/main/java/com/gmail/uprial/masochisticsurvival/common/AngerHelper.java
+++ src/main/java/com/gmail/uprial/masochisticsurvival/common/AngerHelper.java
@@ -4,7 +4,6 @@ import org.bukkit.Chunk;
 import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
